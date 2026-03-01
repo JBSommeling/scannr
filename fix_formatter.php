9075513ec3bfbccfee01092457bf4bb806166ad0
jbsommeling <j.b.sommeling@hotmail.com>
2026-03-02 00:34:31 +0100

    add unit tests for ScanStatistics, UrlNormalizer, and HttpChecker services

--- fix_formatter.php
+++
@@ -1,9 +0,0 @@
-<?php
-// fix_formatter.php
-$file = __DIR__ . '/app/Services/ResultFormatterService.php';
-$f = file_get_contents($file);
-$f = str_replace('$this->scannerService->', '$this->scanStatistics->', $f);
-file_put_contents($file, $f);
-echo "Done: " . substr_count($f, 'scanStatistics') . " refs\n";
-echo "Remaining scannerService: " . substr_count($f, 'scannerService') . "\n";
-
